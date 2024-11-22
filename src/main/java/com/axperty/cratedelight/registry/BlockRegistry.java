55bd6a08c2bc4d3c648aace3d79a24dd373a48a8
axperty <kevingelhorn0@gmail.com>
2024-11-22 12:58:20 -0300

    Fixed beetroot crate, and added version support.

--- src/main/java/com/axperty/cratedelight/registry/BlockRegistry.java
+++ src/main/java/com/axperty/cratedelight/registry/BlockRegistry.java
@@ -4,178 +4,105 @@ import com.axperty.cratedelight.CrateDelight;
 import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
 
 public class BlockRegistry {
 
-    // Carrot Crate Registry
-    public static final Block CARROT_CRATE = registerBlock("carrot_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block CARROT_CRATE = registerBlock("carrot_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Potato Crate Registry
-    public static final Block POTATO_CRATE = registerBlock("potato_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block POTATO_CRATE = registerBlock("potato_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Beetroot Crate Registry
-    public static final Block BEETROOT_CRATE = registerBlock("beetroot_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block BEETROOT_CRATE = registerBlock("beetroot_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Apple Crate Registry
-    public static final Block APPLE_CRATE = registerBlock("apple_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block APPLE_CRATE = registerBlock("apple_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Diamond Apple Crate Registry (ya017zan's Diamond Apples)
-    public static final Block DIAMOND_APPLE_CRATE = registerBlock("diamond_apple_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block DIAMOND_APPLE_CRATE = registerBlock("diamond_apple_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Berry Crate Registry
-    public static final Block BERRY_CRATE = registerBlock("berry_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block BERRY_CRATE = registerBlock("berry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Blueberry Crate Registry (nutritious-feast, moreberries)
-        public static final Block BLUEBERRY_CRATE = registerBlock("blueberry_crate",
-                new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block BLUEBERRY_CRATE = registerBlock("blueberry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Black Berry Crate Registry (moreberries)
-    public static final Block BLACK_BERRY_CRATE = registerBlock("black_berry_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block BLACK_BERRY_CRATE = registerBlock("black_berry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Green Berry Crate Registry (moreberries)
-    public static final Block GREEN_BERRY_CRATE = registerBlock("green_berry_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block GREEN_BERRY_CRATE = registerBlock("green_berry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Orange Berry Crate Registry (moreberries)
-    public static final Block ORANGE_BERRY_CRATE = registerBlock("orange_berry_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block ORANGE_BERRY_CRATE = registerBlock("orange_berry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Yellow Berry Crate Registry (moreberries)
-    public static final Block YELLOW_BERRY_CRATE = registerBlock("yellow_berry_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block YELLOW_BERRY_CRATE = registerBlock("yellow_berry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Purple Berry Crate Registry (moreberries)
-    public static final Block PURPLE_BERRY_CRATE = registerBlock("purple_berry_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block PURPLE_BERRY_CRATE = registerBlock("purple_berry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Glow Berry Crate Registry
-    public static final Block GLOWBERRY_CRATE = registerBlock("glowberry_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS).luminance((state) -> 13)));
+    public static final Block GLOWBERRY_CRATE = registerBlock("glowberry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS).luminance((state) -> 13));
 
-    // Egg Crate Registry
-    public static final Block EGG_CRATE = registerBlock("egg_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block EGG_CRATE = registerBlock("egg_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Duck Egg Crate Registry (naturalist)
-    public static final Block DUCK_EGG_CRATE = registerBlock("duck_egg_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block DUCK_EGG_CRATE = registerBlock("duck_egg_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Peanut Crate Registry (expandeddelight)
-    public static final Block PEANUT_CRATE = registerBlock("peanut_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block PEANUT_CRATE = registerBlock("peanut_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Kiwi Egg Crate Registry (jagmskiwis)
-    public static final Block KIWI_EGG_CRATE = registerBlock("kiwi_egg_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block KIWI_EGG_CRATE = registerBlock("kiwi_egg_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Kiwifruit Crate Registry
-    public static final Block KIWIFRUIT_CRATE = registerBlock("kiwifruit_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block KIWIFRUIT_CRATE = registerBlock("kiwifruit_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Salmon Crate Registry
-    public static final Block SALMON_CRATE = registerBlock("salmon_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block SALMON_CRATE = registerBlock("salmon_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Cod Crate Registry
-    public static final Block COD_CRATE = registerBlock("cod_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block COD_CRATE = registerBlock("cod_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Cat Fish Crate Registry
-    public static final Block CATFISH_CRATE = registerBlock("catfish_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block CATFISH_CRATE = registerBlock("catfish_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Bass Crate Registry
-    public static final Block BASS_CRATE = registerBlock("bass_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block BASS_CRATE = registerBlock("bass_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // BetterEnd Compatibility
-    public static final Block END_FISH_CRATE = registerBlock("end_fish_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block END_FISH_CRATE = registerBlock("end_fish_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Red Mushroom Crate Registry
-    public static final Block RED_MUSHROOM_CRATE = registerBlock("red_mushroom_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block RED_MUSHROOM_CRATE = registerBlock("red_mushroom_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Brown Mushroom Crate Registry
-    public static final Block BROWN_MUSHROOM_CRATE = registerBlock("brown_mushroom_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block BROWN_MUSHROOM_CRATE = registerBlock("brown_mushroom_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Golden Carrot Crate Registry
-    public static final Block GOLDEN_CARROT_CRATE = registerBlock("golden_carrot_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block GOLDEN_CARROT_CRATE = registerBlock("golden_carrot_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Golden Apple Crate Registry
-    public static final Block GOLDEN_APPLE_CRATE = registerBlock("golden_apple_crate",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block GOLDEN_APPLE_CRATE = registerBlock("golden_apple_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    // Cocoa Beans Bag Registry
-    public static final Block COCOABEANS_BAG = registerBlock("cocoabeans_bag",
-            new Block(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)));
+    public static final Block COCOABEANS_BAG = registerBlock("cocoabeans_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
-    // Sugar Bag Registry
-    public static final Block SUGAR_BAG = registerBlock("sugar_bag",
-            new Block(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)));
+    public static final Block SUGAR_BAG = registerBlock("sugar_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
-    // Gunpowder Bag Registry
-    public static final Block GUNPOWDER_BAG = registerBlock("gunpowder_bag",
-            new Block(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)));
+    public static final Block GUNPOWDER_BAG = registerBlock("gunpowder_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
-    // Cookie Bag Registry
-    public static final Block COOKIE_BAG = registerBlock("cookie_bag",
-            new Block(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)));
+    public static final Block COOKIE_BAG = registerBlock("cookie_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
-    // Salt Bag Registry
-    public static final Block SALT_BAG = registerBlock("salt_bag",
-            new Block(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)));
-    // Cinnamon Bag Registry
-    public static final Block GROUND_CINNAMON_BAG = registerBlock("ground_cinnamon_bag",
-            new Block(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)));
+    public static final Block SALT_BAG = registerBlock("salt_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
-    // Wheat Flour Bag Registry
-    public static final Block WHEAT_FLOUR_BAG = registerBlock("wheat_flour_bag",
-            new Block(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)));
+    public static final Block GROUND_CINNAMON_BAG = registerBlock("ground_cinnamon_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
-    // Powdered Obsidian Bag Registry
-    public static final Block POWDERED_OBSIDIAN_BAG = registerBlock("powdered_obsidian_bag",
-            new Block(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)));
+    public static final Block WHEAT_FLOUR_BAG = registerBlock("wheat_flour_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
-    // Cinder Flour Bag Registry
-    public static final Block CINDER_FLOUR_BAG = registerBlock("cinder_flour_bag",
-            new Block(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)));
+    public static final Block POWDERED_OBSIDIAN_BAG = registerBlock("powdered_obsidian_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
-    // End Dust Bag Registry
-    public static final Block ENDER_DUST_BAG = registerBlock("ender_dust_bag",
-            new Block(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)));
+    public static final Block CINDER_FLOUR_BAG = registerBlock("cinder_flour_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
-    // Stacked Melons Registry
-    public static final Block STACKED_MELONS = registerBlock("stacked_melons",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
+    public static final Block ENDER_DUST_BAG = registerBlock("ender_dust_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
-    // Stacked Pumpkins Registry
-    public static final Block STACKED_PUMPKINS = registerBlock("stacked_pumpkins",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
-
-    private static Block registerBlock(String name, Block block) {
-        registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, Identifier.tryParse(CrateDelight.MODID + ":" + name), block);
+    private static Block registerBlock(String path, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
+        final Identifier identifier = Identifier.of(CrateDelight.MODID, path);
+        final RegistryKey<Block> registryKey = RegistryKey.of(RegistryKeys.BLOCK, identifier);
+        final Block block = Blocks.register(registryKey, factory, settings);
+        registerItem(path, itemSettings -> new BlockItem(block, itemSettings), new Item.Settings());
+        return block;
     }
 
-    private static Item registerBlockItem(String name, Block block) {
-        return Registry.register(Registries.ITEM, Identifier.tryParse(CrateDelight.MODID + ":" + name),
-                new BlockItem(block, new Item.Settings()));
+    public static Item registerItem(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CrateDelight.MODID, path));
+        return Items.register(registryKey, factory, settings);
     }
 
     public static void registerModBlocks() {
-        CrateDelight.LOGGER.info("Registering ModBLocks for " + CrateDelight.MODID);
+        CrateDelight.LOGGER.info("Registering mod blocks for " + CrateDelight.MODID);
     }
 }
