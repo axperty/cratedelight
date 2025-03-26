8f80923d72a6a639abc87ff86de273e42afe9b15
axperty <kevingelhorn0@gmail.com>
2025-03-26 01:09:31 -0300

    Added villager trades.

---
+++ src/main/java/com/axperty/cratedelight/event/VillagerEvents.java
@@ -0,0 +1,49 @@
+package com.axperty.cratedelight.event;
+
+import com.axperty.cratedelight.CrateDelight;
+import com.axperty.cratedelight.registry.BlockRegistry;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.BasicItemListing;
+import net.neoforged.neoforge.event.village.VillagerTradesEvent;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+
+@EventBusSubscriber(modid = CrateDelight.MODID)
+@ParametersAreNonnullByDefault
+public class VillagerEvents {
+    public VillagerEvents()
+    {
+
+    }
+
+    @SubscribeEvent
+    public static void onVillagerTrades(VillagerTradesEvent event) {
+        Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+        VillagerProfession profession = event.getType();
+        ResourceLocation professionKey = BuiltInRegistries.VILLAGER_PROFESSION.getKey(profession);
+        if (professionKey.getPath().equals("farmer")) {
+            trades.get(1).add(emeraldForItemsTrade(BlockRegistry.CARROT_CRATE.get(), 3, 1, 2));
+            trades.get(1).add(emeraldForItemsTrade(BlockRegistry.POTATO_CRATE.get(), 3, 1, 2));
+            trades.get(1).add(emeraldForItemsTrade(BlockRegistry.APPLE_CRATE.get(), 3, 1, 2));
+
+        }
+    }
+
+    public static BasicItemListing emeraldForItemsTrade(ItemLike item, int count, int maxTrades, int xp) {
+        return new BasicItemListing(new ItemStack(item, count), new ItemStack(Items.EMERALD,  9), maxTrades, xp, 0.05F);
+    }
+
+    public static BasicItemListing itemForEmeraldTrade(ItemLike item, int count, int maxTrades, int xp) {
+        return new BasicItemListing(9, new ItemStack(item, count), maxTrades, xp, 0.05F);
+    }
+}
