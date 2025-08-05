856ab522be4d4a766a02c25fa6c0bc65c268c384
axperty <kevingelhorn0@gmail.com>
2025-08-05 17:15:40 -0300

    See changelog.md for the latest changes.

--- src/main/java/com/axperty/cratedelight/item/ModCreativeTab.java
+++ src/main/java/com/axperty/cratedelight/item/ModCreativeTab.java
@@ -21,80 +21,42 @@ public class ModCreativeTab {
 
                         if (!FabricLoader.getInstance().isModLoaded("farmersdelight")) {
                             entries.add(BlockRegistry.CARROT_CRATE);
-                            entries.add(BlockRegistry.POTATO_CRATE);
                             entries.add(BlockRegistry.BEETROOT_CRATE);
+                            entries.add(BlockRegistry.POTATO_CRATE);
                         }
 
+                        entries.add(BlockRegistry.POISONOUS_POTATO_CRATE);
                         entries.add(BlockRegistry.APPLE_CRATE);
-
-                        if (FabricLoader.getInstance().isModLoaded("diamond_apples")) {
-                            entries.add(BlockRegistry.DIAMOND_APPLE_CRATE);
-                        }
-
                         entries.add(BlockRegistry.BERRY_CRATE);
-
-                        if (FabricLoader.getInstance().isModLoaded("nutritious-feast") || FabricLoader.getInstance().isModLoaded("moreberries")) {
-                            entries.add(BlockRegistry.BLUEBERRY_CRATE);
-                        }
-
-                        if (FabricLoader.getInstance().isModLoaded("moreberries")) {
-                            entries.add(BlockRegistry.BLACK_BERRY_CRATE);
-                            entries.add(BlockRegistry.GREEN_BERRY_CRATE);
-                            entries.add(BlockRegistry.ORANGE_BERRY_CRATE);
-                            entries.add(BlockRegistry.YELLOW_BERRY_CRATE);
-                            entries.add(BlockRegistry.PURPLE_BERRY_CRATE);
-                        }
-
                         entries.add(BlockRegistry.GLOWBERRY_CRATE);
                         entries.add(BlockRegistry.EGG_CRATE);
-
-                        if (FabricLoader.getInstance().isModLoaded("naturalist")) {
-                            entries.add(BlockRegistry.DUCK_EGG_CRATE);
-                        }
+                        entries.add(BlockRegistry.BROWN_EGG_CRATE);
+                        entries.add(BlockRegistry.BLUE_EGG_CRATE);
 
                         if (FabricLoader.getInstance().isModLoaded("expandeddelight")) {
                             entries.add(BlockRegistry.PEANUT_CRATE);
                         }
 
-                        if (FabricLoader.getInstance().isModLoaded("jagmkiwis")) {
-                            entries.add(BlockRegistry.KIWI_EGG_CRATE);
-                            entries.add(BlockRegistry.KIWIFRUIT_CRATE);
-                        }
-
                         entries.add(BlockRegistry.SALMON_CRATE);
                         entries.add(BlockRegistry.COD_CRATE);
-
-                        if (FabricLoader.getInstance().isModLoaded("naturalist")) {
-                            entries.add(BlockRegistry.CATFISH_CRATE);
-                            entries.add(BlockRegistry.BASS_CRATE);
-                        }
-
-                        if (FabricLoader.getInstance().isModLoaded("betterend")) {
-                            entries.add(BlockRegistry.END_FISH_CRATE);
-                        }
-
                         entries.add(BlockRegistry.RED_MUSHROOM_CRATE);
                         entries.add(BlockRegistry.BROWN_MUSHROOM_CRATE);
+
+                        if (FabricLoader.getInstance().isModLoaded("farmersdelight")) {
+                            entries.add(BlockRegistry.PUMPKIN_SLICE_CRATE);
+                        }
+
                         entries.add(BlockRegistry.GOLDEN_CARROT_CRATE);
                         entries.add(BlockRegistry.GOLDEN_APPLE_CRATE);
+                        entries.add(BlockRegistry.BREAD_BAG);
+                        entries.add(BlockRegistry.COOKIE_BAG);
                         entries.add(BlockRegistry.COCOABEANS_BAG);
                         entries.add(BlockRegistry.SUGAR_BAG);
                         entries.add(BlockRegistry.GUNPOWDER_BAG);
-                        entries.add(BlockRegistry.COOKIE_BAG);
 
                         if (FabricLoader.getInstance().isModLoaded("expandeddelight")) {
                             entries.add(BlockRegistry.SALT_BAG);
                             entries.add(BlockRegistry.GROUND_CINNAMON_BAG);
-                        }
-
-                        if (FabricLoader.getInstance().isModLoaded("create")) {
-                            entries.add(BlockRegistry.WHEAT_FLOUR_BAG);
-                            entries.add(BlockRegistry.POWDERED_OBSIDIAN_BAG);
-                            entries.add(BlockRegistry.CINDER_FLOUR_BAG);
-                        }
-
-                        if (FabricLoader.getInstance().isModLoaded("betterend")) {
-                            entries.add(BlockRegistry.ENDER_DUST_BAG);
                         }
                     })
                     .build());
--- src/main/java/com/axperty/cratedelight/registry/BlockRegistry.java
+++ src/main/java/com/axperty/cratedelight/registry/BlockRegistry.java
@@ -15,57 +15,43 @@ public class BlockRegistry {
 
     public static final Block CARROT_CRATE = registerBlock("carrot_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
+    public static final Block BEETROOT_CRATE = registerBlock("beetroot_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
+
     public static final Block POTATO_CRATE = registerBlock("potato_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    public static final Block BEETROOT_CRATE = registerBlock("beetroot_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
+    public static final Block POISONOUS_POTATO_CRATE = registerBlock("poisonous_potato_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
     public static final Block APPLE_CRATE = registerBlock("apple_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    public static final Block DIAMOND_APPLE_CRATE = registerBlock("diamond_apple_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
-
     public static final Block BERRY_CRATE = registerBlock("berry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
-
-    public static final Block BLUEBERRY_CRATE = registerBlock("blueberry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
-
-    public static final Block BLACK_BERRY_CRATE = registerBlock("black_berry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
-
-    public static final Block GREEN_BERRY_CRATE = registerBlock("green_berry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
-
-    public static final Block ORANGE_BERRY_CRATE = registerBlock("orange_berry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
-
-    public static final Block YELLOW_BERRY_CRATE = registerBlock("yellow_berry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
-
-    public static final Block PURPLE_BERRY_CRATE = registerBlock("purple_berry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
     public static final Block GLOWBERRY_CRATE = registerBlock("glowberry_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS).luminance((state) -> 13));
 
     public static final Block EGG_CRATE = registerBlock("egg_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    public static final Block DUCK_EGG_CRATE = registerBlock("duck_egg_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
+    public static final Block BROWN_EGG_CRATE = registerBlock("brown_egg_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
+
+    public static final Block BLUE_EGG_CRATE = registerBlock("blue_egg_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
     public static final Block PEANUT_CRATE = registerBlock("peanut_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
-
-    public static final Block KIWI_EGG_CRATE = registerBlock("kiwi_egg_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
-
-    public static final Block KIWIFRUIT_CRATE = registerBlock("kiwifruit_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
     public static final Block SALMON_CRATE = registerBlock("salmon_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
     public static final Block COD_CRATE = registerBlock("cod_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
-    public static final Block CATFISH_CRATE = registerBlock("catfish_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
-
-    public static final Block BASS_CRATE = registerBlock("bass_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
-
-    public static final Block END_FISH_CRATE = registerBlock("end_fish_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
-
     public static final Block RED_MUSHROOM_CRATE = registerBlock("red_mushroom_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
     public static final Block BROWN_MUSHROOM_CRATE = registerBlock("brown_mushroom_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
+    public static final Block PUMPKIN_SLICE_CRATE = registerBlock("pumpkin_slice_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
+
     public static final Block GOLDEN_CARROT_CRATE = registerBlock("golden_carrot_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
 
     public static final Block GOLDEN_APPLE_CRATE = registerBlock("golden_apple_crate", Block::new, Block.Settings.copy(Blocks.OAK_PLANKS));
+
+    public static final Block BREAD_BAG = registerBlock("bread_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
+
+    public static final Block COOKIE_BAG = registerBlock("cookie_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
     public static final Block COCOABEANS_BAG = registerBlock("cocoabeans_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
@@ -73,20 +59,9 @@ public class BlockRegistry {
 
     public static final Block GUNPOWDER_BAG = registerBlock("gunpowder_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
-    public static final Block COOKIE_BAG = registerBlock("cookie_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
-
     public static final Block SALT_BAG = registerBlock("salt_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
 
     public static final Block GROUND_CINNAMON_BAG = registerBlock("ground_cinnamon_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
-
-    public static final Block WHEAT_FLOUR_BAG = registerBlock("wheat_flour_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
-
-    public static final Block POWDERED_OBSIDIAN_BAG = registerBlock("powdered_obsidian_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
-
-    public static final Block CINDER_FLOUR_BAG = registerBlock("cinder_flour_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
-
-    public static final Block ENDER_DUST_BAG = registerBlock("ender_dust_bag", Block::new, Block.Settings.copy(Blocks.WHITE_WOOL));
-
     private static Block registerBlock(String path, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
         final Identifier identifier = Identifier.of(CrateDelight.MODID, path);
         final RegistryKey<Block> registryKey = RegistryKey.of(RegistryKeys.BLOCK, identifier);
