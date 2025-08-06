fc7e948797c0281780b0411f7b033041d511abfa
axperty <kevingelhorn0@gmail.com>
2025-08-06 18:40:23 -0300

    See changelog.md for the latest changes.

--- changelog.md
+++ changelog.md
@@ -4,7 +4,13 @@
 
 ### ✨ What's New:
 
-- Added support for Minecraft 1.21.4.
+- Added Poisonous Potato Crate.
+- Added Brown Egg Crate.
+- Added Blue Egg Crate.
+- Added Bread Bag.
+- Added Pumpkin Slice Crate (for Farmer's Delight).
+- Potato Crates can now be smelted in a furnace to get 9 potato items.
+
 ***
 
 Send your suggestions or report any issues on [GitHub](https://github.com/axperty/cratedelight/issues/new). Support Crate Delight by [creating your own server](https://url-shortener.curseforge.com/p5i2k) or [donating on PayPal](https://paypal.me/kevgelhorn). 
--- src/main/java/com/axperty/cratedelight/registry/CreativeTabRegistry.java
+++ src/main/java/com/axperty/cratedelight/registry/CreativeTabRegistry.java
@@ -20,64 +20,41 @@ public class CreativeTabRegistry {
             .displayItems((parameters, output) -> {
                 if (!ModList.get().isLoaded("farmersdelight")) {
                     output.accept(BlockRegistry.CARROT_CRATE.get());
-                    output.accept(BlockRegistry.POTATO_CRATE.get());
                     output.accept(BlockRegistry.BEETROOT_CRATE.get());
+                    output.accept(BlockRegistry.POTATO_CRATE.get());
                 }
+
+                output.accept(BlockRegistry.POISONOUS_POTATO_CRATE.get());
 
                 output.accept(BlockRegistry.APPLE_CRATE.get());
                 output.accept(BlockRegistry.BERRY_CRATE.get());
-
-                if (ModList.get().isLoaded("nutritious_feast")) {
-                    output.accept(BlockRegistry.BLUEBERRY_CRATE.get());
-                }
-
                 output.accept(BlockRegistry.GLOWBERRY_CRATE.get());
                 output.accept(BlockRegistry.EGG_CRATE.get());
                 output.accept(BlockRegistry.SALMON_CRATE.get());
                 output.accept(BlockRegistry.COD_CRATE.get());
 
-                if (ModList.get().isLoaded("naturalist")) {
-                    output.accept(BlockRegistry.DUCK_EGG_CRATE.get());
-                    output.accept(BlockRegistry.CATFISH_CRATE.get());
-                    output.accept(BlockRegistry.BASS_CRATE.get());
-                }
-
                 if (ModList.get().isLoaded("expandeddelight")) {
                     output.accept(BlockRegistry.PEANUT_CRATE.get());
                 }
 
-                if (ModList.get().isLoaded("jagmkiwis")) {
-                    output.accept(BlockRegistry.KIWI_EGG_CRATE.get());
-                    output.accept(BlockRegistry.KIWIFRUIT_CRATE.get());
-                }
-
-                if (ModList.get().isLoaded("alexsmobs")) {
-                    output.accept(BlockRegistry.EMU_EGG_CRATE.get());
-                    output.accept(BlockRegistry.TERRAPIN_EGG_CRATE.get());
-                    output.accept(BlockRegistry.CROCODILE_EGG_CRATE.get());
-                    output.accept(BlockRegistry.CAIMAN_EGG_CRATE.get());
-                    output.accept(BlockRegistry.PLATYPUS_EGG_CRATE.get());
-                    output.accept(BlockRegistry.BANANA_CRATE.get());
-                }
-
                 output.accept(BlockRegistry.RED_MUSHROOM_CRATE.get());
                 output.accept(BlockRegistry.BROWN_MUSHROOM_CRATE.get());
+
+                if (ModList.get().isLoaded("farmersdelight")) {
+                    output.accept(BlockRegistry.PUMPKIN_SLICE_CRATE.get());
+                }
+
                 output.accept(BlockRegistry.GOLDEN_CARROT_CRATE.get());
                 output.accept(BlockRegistry.GOLDEN_APPLE_CRATE.get());
+                output.accept(BlockRegistry.BREAD_BAG);
+                output.accept(BlockRegistry.COOKIE_BAG.get());
                 output.accept(BlockRegistry.COCOABEANS_BAG.get());
                 output.accept(BlockRegistry.SUGAR_BAG.get());
-                output.accept(BlockRegistry.COOKIE_BAG.get());
                 output.accept(BlockRegistry.GUNPOWDER_BAG.get());
 
                 if (ModList.get().isLoaded("expandeddelight")) {
                     output.accept(BlockRegistry.SALT_BAG.get());
                     output.accept(BlockRegistry.GROUND_CINNAMON_BAG.get());
-                }
-
-                if (ModList.get().isLoaded("create")) {
-                    output.accept(BlockRegistry.WHEAT_FLOUR_BAG.get());
-                    output.accept(BlockRegistry.POWDERED_OBSIDIAN_BAG.get());
-                    output.accept(BlockRegistry.CINDER_FLOUR_BAG.get());
                 }
             }).build());
 }
