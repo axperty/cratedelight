513fd268d71253e577efa8601cef9bcc70e825e8
axperty <kevingelhorn0@gmail.com>
2025-08-07 11:43:17 -0300

    See changelog.md for the latest changes.

--- src/main/java/com/axperty/cratedelight/registry/BlockRegistry.java
+++ src/main/java/com/axperty/cratedelight/registry/BlockRegistry.java
@@ -19,6 +19,10 @@ public class BlockRegistry {
     public static final DeferredBlock<Block> POTATO_CRATE = BLOCKS.registerSimpleBlock("potato_crate",
             BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
+    // Poisonous Potato Crate
+    public static final DeferredBlock<Block> POISONOUS_POTATO_CRATE = BLOCKS.registerSimpleBlock("poisonous_potato_crate",
+            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+
     // Beetroot Crate
     public static final DeferredBlock<Block> BEETROOT_CRATE = BLOCKS.registerSimpleBlock("beetroot_crate",
             BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
@@ -31,10 +35,6 @@ public class BlockRegistry {
     public static final DeferredBlock<Block> BERRY_CRATE = BLOCKS.registerSimpleBlock("berry_crate",
             BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
-    // Blueberry Crate (Nutritious Feast)
-    public static final DeferredBlock<Block> BLUEBERRY_CRATE = BLOCKS.registerSimpleBlock("blueberry_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
-
     // Glow Berry Crate
     public static final DeferredBlock<Block> GLOWBERRY_CRATE = BLOCKS.registerSimpleBlock("glowberry_crate",
             BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD).lightLevel((state) -> 13));
@@ -43,44 +43,8 @@ public class BlockRegistry {
     public static final DeferredBlock<Block> EGG_CRATE = BLOCKS.registerSimpleBlock("egg_crate",
             BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
-    // Duck Egg Crate (Naturalist)
-    public static final DeferredBlock<Block> DUCK_EGG_CRATE = BLOCKS.registerSimpleBlock("duck_egg_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
-
     // Peanut Crate (Expanded Delight)
     public static final DeferredBlock<Block> PEANUT_CRATE = BLOCKS.registerSimpleBlock("peanut_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
-
-    // Emu Egg Crate (Alex's Mobs)
-    public static final DeferredBlock<Block> EMU_EGG_CRATE = BLOCKS.registerSimpleBlock("emu_egg_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
-
-    // Terrapin Egg Crate (Alex's Mobs)
-    public static final DeferredBlock<Block> TERRAPIN_EGG_CRATE = BLOCKS.registerSimpleBlock("terrapin_egg_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
-
-    // Crocodile Egg Crate (Alex's Mobs)
-    public static final DeferredBlock<Block> CROCODILE_EGG_CRATE = BLOCKS.registerSimpleBlock("crocodile_egg_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
-
-    // Caiman Egg Crate (Alex's Mobs)
-    public static final DeferredBlock<Block> CAIMAN_EGG_CRATE = BLOCKS.registerSimpleBlock("caiman_egg_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
-
-    // Platypus Egg Crate (Alex's Mobs) (Perry The Platypus!)
-    public static final DeferredBlock<Block> PLATYPUS_EGG_CRATE = BLOCKS.registerSimpleBlock("platypus_egg_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
-
-    // Kiwi Egg Crate (Jagm's Kiwis)
-    public static final DeferredBlock<Block> KIWI_EGG_CRATE = BLOCKS.registerSimpleBlock("kiwi_egg_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
-
-    // Kiwifruit Crate (Jagm's Kiwis)
-    public static final DeferredBlock<Block> KIWIFRUIT_CRATE = BLOCKS.registerSimpleBlock("kiwifruit_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
-
-    // Banana Crate (Alex's Mobs)
-    public static final DeferredBlock<Block> BANANA_CRATE = BLOCKS.registerSimpleBlock("banana_crate",
             BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Salmon Crate
@@ -91,20 +55,16 @@ public class BlockRegistry {
     public static final DeferredBlock<Block> COD_CRATE = BLOCKS.registerSimpleBlock("cod_crate",
             BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
-    // Catfish Crate (Naturalist)
-    public static final DeferredBlock<Block> CATFISH_CRATE = BLOCKS.registerSimpleBlock("catfish_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
-
-    // Bass Crate (Naturalist)
-    public static final DeferredBlock<Block> BASS_CRATE = BLOCKS.registerSimpleBlock("bass_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
-
     // Red Mushroom Crate
     public static final DeferredBlock<Block> RED_MUSHROOM_CRATE = BLOCKS.registerSimpleBlock("red_mushroom_crate",
             BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Brown Mushroom Crate
     public static final DeferredBlock<Block> BROWN_MUSHROOM_CRATE = BLOCKS.registerSimpleBlock("brown_mushroom_crate",
+            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+
+    // Pumpkin Slice Crate
+    public static final DeferredBlock<Block> PUMPKIN_SLICE_CRATE = BLOCKS.registerSimpleBlock("pumpkin_slice_crate",
             BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Golden Carrot Crate
@@ -114,6 +74,14 @@ public class BlockRegistry {
     // Golden Apple Crate
     public static final DeferredBlock<Block> GOLDEN_APPLE_CRATE = BLOCKS.registerSimpleBlock("golden_apple_crate",
             BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+
+    // Bread Bag
+    public static final DeferredBlock<Block> BREAD_BAG = BLOCKS.registerSimpleBlock("bread_bag",
+            BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
+
+    // Cookie Bag
+    public static final DeferredBlock<Block> COOKIE_BAG = BLOCKS.registerSimpleBlock("cookie_bag",
+            BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
 
     // Cocoa Beans Bag
     public static final DeferredBlock<Block> COCOABEANS_BAG = BLOCKS.registerSimpleBlock("cocoabeans_bag",
@@ -133,22 +101,6 @@ public class BlockRegistry {
 
     // Ground Cinnamon Bag (Expanded Delight)
     public static final DeferredBlock<Block> GROUND_CINNAMON_BAG = BLOCKS.registerSimpleBlock("ground_cinnamon_bag",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
-
-    // Wheat Flour Bag (Create)
-    public static final DeferredBlock<Block> WHEAT_FLOUR_BAG = BLOCKS.registerSimpleBlock("wheat_flour_bag",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
-
-    // Powdered Obsidian Bag (Create)
-    public static final DeferredBlock<Block> POWDERED_OBSIDIAN_BAG = BLOCKS.registerSimpleBlock("powdered_obsidian_bag",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
-
-    // Cinder Flour Bag (Create)
-    public static final DeferredBlock<Block> CINDER_FLOUR_BAG = BLOCKS.registerSimpleBlock("cinder_flour_bag",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
-
-    // Cookie Bag
-    public static final DeferredBlock<Block> COOKIE_BAG = BLOCKS.registerSimpleBlock("cookie_bag",
             BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
 
 }
--- src/main/java/com/axperty/cratedelight/registry/ItemRegistry.java
+++ src/main/java/com/axperty/cratedelight/registry/ItemRegistry.java
@@ -11,11 +11,14 @@ public class ItemRegistry {
     // Carrot Crate Item
     public static final DeferredItem<BlockItem> CARROT_CRATE_ITEM = ITEMS.registerSimpleBlockItem("carrot_crate", BlockRegistry.CARROT_CRATE);
 
+    // Beetroot Crate Item
+    public static final DeferredItem<BlockItem> BEETROOT_CRATE_ITEM = ITEMS.registerSimpleBlockItem("beetroot_crate", BlockRegistry.BEETROOT_CRATE);
+
     // Potato Crate Item
     public static final DeferredItem<BlockItem> POTATO_CRATE_ITEM = ITEMS.registerSimpleBlockItem("potato_crate", BlockRegistry.POTATO_CRATE);
 
-    // Beetroot Crate Item
-    public static final DeferredItem<BlockItem> BEETROOT_CRATE_ITEM = ITEMS.registerSimpleBlockItem("beetroot_crate", BlockRegistry.BEETROOT_CRATE);
+    // Poisonous Potato Crate Item
+    public static final DeferredItem<BlockItem> POISONOUS_POTATO_CRATE_ITEM = ITEMS.registerSimpleBlockItem("poisonous_potato_crate", BlockRegistry.POISONOUS_POTATO_CRATE);
 
     // Apple Crate Item
     public static final DeferredItem<BlockItem> APPLE_CRATE_ITEM = ITEMS.registerSimpleBlockItem("apple_crate", BlockRegistry.APPLE_CRATE);
@@ -23,44 +26,14 @@ public class ItemRegistry {
     // Berry Crate Item
     public static final DeferredItem<BlockItem> BERRY_CRATE_ITEM = ITEMS.registerSimpleBlockItem("berry_crate", BlockRegistry.BERRY_CRATE);
 
-    // Blueberry Crate Item
-    public static final DeferredItem<BlockItem> BLUEBERRY_CRATE_ITEM = ITEMS.registerSimpleBlockItem("blueberry_crate", BlockRegistry.BLUEBERRY_CRATE);
-
     // Glow Berry Crate Item
     public static final DeferredItem<BlockItem> GLOWBERRY_CRATE_ITEM = ITEMS.registerSimpleBlockItem("glowberry_crate", BlockRegistry.GLOWBERRY_CRATE);
 
     // Egg Crate Item
     public static final DeferredItem<BlockItem> EGG_CRATE_ITEM = ITEMS.registerSimpleBlockItem("egg_crate", BlockRegistry.EGG_CRATE);
 
-    // Duck Egg Crate Item
-    public static final DeferredItem<BlockItem> DUCK_EGG_CRATE_ITEM = ITEMS.registerSimpleBlockItem("duck_egg_crate", BlockRegistry.DUCK_EGG_CRATE);
-
     // Peanut Egg Crate Item
     public static final DeferredItem<BlockItem> PEANUT_CRATE_ITEM = ITEMS.registerSimpleBlockItem("peanut_crate", BlockRegistry.PEANUT_CRATE);
-
-    // Emu Egg Crate Item
-    public static final DeferredItem<BlockItem> EMU_EGG_CRATE_ITEM = ITEMS.registerSimpleBlockItem("emu_egg_crate", BlockRegistry.EMU_EGG_CRATE);
-
-    // Terrapin Egg Crate Item
-    public static final DeferredItem<BlockItem> TERRAPIN_EGG_CRATE_ITEM = ITEMS.registerSimpleBlockItem("terrapin_egg_crate", BlockRegistry.TERRAPIN_EGG_CRATE);
-
-    // Crocodile Egg Crate Item
-    public static final DeferredItem<BlockItem> CROCODILE_EGG_CRATE_ITEM = ITEMS.registerSimpleBlockItem("crocodile_egg_crate", BlockRegistry.CROCODILE_EGG_CRATE);
-
-    // Caiman Egg Crate Item
-    public static final DeferredItem<BlockItem> CAIMAN_EGG_CRATE_ITEM = ITEMS.registerSimpleBlockItem("caiman_egg_crate", BlockRegistry.CAIMAN_EGG_CRATE);
-
-    // Platypus Egg Crate Item
-    public static final DeferredItem<BlockItem> PLATYPUS_EGG_CRATE_ITEM = ITEMS.registerSimpleBlockItem("platypus_egg_crate", BlockRegistry.PLATYPUS_EGG_CRATE);
-
-    // Kiwi Egg Crate Item
-    public static final DeferredItem<BlockItem> KIWI_EGG_CRATE_ITEM = ITEMS.registerSimpleBlockItem("kiwi_egg_crate", BlockRegistry.KIWI_EGG_CRATE);
-
-    // Kiwifruit Crate Item
-    public static final DeferredItem<BlockItem> KIWIFRUIT_CRATE_ITEM = ITEMS.registerSimpleBlockItem("kiwifruit_crate", BlockRegistry.KIWIFRUIT_CRATE);
-
-    // Banana Crate Item
-    public static final DeferredItem<BlockItem> BANANA_CRATE_ITEM = ITEMS.registerSimpleBlockItem("banana_crate", BlockRegistry.BANANA_CRATE);
 
     // Salmon Crate Item
     public static final DeferredItem<BlockItem> SALMON_CRATE_ITEM = ITEMS.registerSimpleBlockItem("salmon_crate", BlockRegistry.SALMON_CRATE);
@@ -68,23 +41,26 @@ public class ItemRegistry {
     // Cod Crate Item
     public static final DeferredItem<BlockItem> COD_CRATE_ITEM = ITEMS.registerSimpleBlockItem("cod_crate", BlockRegistry.COD_CRATE);
 
-    // Catfish Crate Item
-    public static final DeferredItem<BlockItem> CATFISH_CRATE_ITEM = ITEMS.registerSimpleBlockItem("catfish_crate", BlockRegistry.CATFISH_CRATE);
-
-    // Bass Crate Item
-    public static final DeferredItem<BlockItem> BASS_CRATE_ITEM = ITEMS.registerSimpleBlockItem("bass_crate", BlockRegistry.BASS_CRATE);
-
     // Red Mushroom Crate Item
     public static final DeferredItem<BlockItem> RED_MUSHROOM_CRATE_ITEM = ITEMS.registerSimpleBlockItem("red_mushroom_crate", BlockRegistry.RED_MUSHROOM_CRATE);
 
     // Brown Mushroom Crate Item
     public static final DeferredItem<BlockItem> BROWN_MUSHROOM_CRATE_ITEM = ITEMS.registerSimpleBlockItem("brown_mushroom_crate", BlockRegistry.BROWN_MUSHROOM_CRATE);
 
+    // Pumpkin Slice Crate Item
+    public static final DeferredItem<BlockItem> PUMPKIN_SLICE_CRATE_ITEM = ITEMS.registerSimpleBlockItem("pumpkin_slice_crate", BlockRegistry.PUMPKIN_SLICE_CRATE);
+
     // Golden Carrot Crate Item
     public static final DeferredItem<BlockItem> GOLDEN_CARROT_CRATE_ITEM = ITEMS.registerSimpleBlockItem("golden_carrot_crate", BlockRegistry.GOLDEN_CARROT_CRATE);
 
     // Golden Apple Crate Item
     public static final DeferredItem<BlockItem> GOLDEN_APPLE_CRATE_ITEM = ITEMS.registerSimpleBlockItem("golden_apple_crate", BlockRegistry.GOLDEN_APPLE_CRATE);
+
+    // Bread Bag Item
+    public static final DeferredItem<BlockItem> BREAD_BAG_ITEM = ITEMS.registerSimpleBlockItem("bread_bag", BlockRegistry.BREAD_BAG);
+
+    // Cookie Bag Item
+    public static final DeferredItem<BlockItem> COOKIE_BAG_ITEM = ITEMS.registerSimpleBlockItem("cookie_bag", BlockRegistry.COOKIE_BAG);
 
     // Cocoa Beans Bag Item
     public static final DeferredItem<BlockItem> COCOABEANS_BAG_ITEM = ITEMS.registerSimpleBlockItem("cocoabeans_bag", BlockRegistry.COCOABEANS_BAG);
@@ -100,16 +76,4 @@ public class ItemRegistry {
 
     // Ground Cinnamon Bag Item
     public static final DeferredItem<BlockItem> GROUND_CINNAMON_BAG_ITEM = ITEMS.registerSimpleBlockItem("ground_cinnamon_bag", BlockRegistry.GROUND_CINNAMON_BAG);
-
-    // Wheat Flour Bag Item
-    public static final DeferredItem<BlockItem> WHEAT_FLOUR_BAG_ITEM = ITEMS.registerSimpleBlockItem("wheat_flour_bag", BlockRegistry.WHEAT_FLOUR_BAG);
-
-    // Powdered Obsidian Bag Item
-    public static final DeferredItem<BlockItem> POWDERED_OBSIDIAN_BAG_ITEM = ITEMS.registerSimpleBlockItem("powdered_obsidian_bag", BlockRegistry.POWDERED_OBSIDIAN_BAG);
-
-    // Cinder Flour Bag Item
-    public static final DeferredItem<BlockItem> CINDER_FLOUR_BAG_ITEM = ITEMS.registerSimpleBlockItem("cinder_flour_bag", BlockRegistry.CINDER_FLOUR_BAG);
-
-    // Cookie Bag Item
-    public static final DeferredItem<BlockItem> COOKIE_BAG_ITEM = ITEMS.registerSimpleBlockItem("cookie_bag", BlockRegistry.COOKIE_BAG);
 }
