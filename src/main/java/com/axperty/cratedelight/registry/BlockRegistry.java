d43983a593245874ca3c1b3bb7e4b0b915725562
axperty <107956734+axperty@users.noreply.github.com>
2026-04-11 13:01:24 -0300

    Fixed issue where Crate Delight wouldn't let the Minecraft instance launch in version 26.1.2 (thanks, SDUBZGAMING!).

--- src/main/java/com/axperty/cratedelight/registry/BlockRegistry.java
+++ src/main/java/com/axperty/cratedelight/registry/BlockRegistry.java
@@ -9,107 +9,106 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
 import static com.axperty.cratedelight.CrateDelight.MODID;
 
-@SuppressWarnings("deprecation")
 public class BlockRegistry {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
 
     // Carrot Crate
-    public static final DeferredBlock<Block> CARROT_CRATE = BLOCKS.registerSimpleBlock("carrot_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> CARROT_CRATE = BLOCKS.registerBlock("carrot_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Beetroot Crate
-    public static final DeferredBlock<Block> BEETROOT_CRATE = BLOCKS.registerSimpleBlock("beetroot_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> BEETROOT_CRATE = BLOCKS.registerBlock("beetroot_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Potato Crate
-    public static final DeferredBlock<Block> POTATO_CRATE = BLOCKS.registerSimpleBlock("potato_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> POTATO_CRATE = BLOCKS.registerBlock("potato_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Poisonous Potato Crate
-    public static final DeferredBlock<Block> POISONOUS_POTATO_CRATE = BLOCKS.registerSimpleBlock("poisonous_potato_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> POISONOUS_POTATO_CRATE = BLOCKS.registerBlock("poisonous_potato_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Apple Crate
-    public static final DeferredBlock<Block> APPLE_CRATE = BLOCKS.registerSimpleBlock("apple_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> APPLE_CRATE = BLOCKS.registerBlock("apple_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Berry Crate
-    public static final DeferredBlock<Block> BERRY_CRATE = BLOCKS.registerSimpleBlock("berry_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> BERRY_CRATE = BLOCKS.registerBlock("berry_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Glow Berry Crate
-    public static final DeferredBlock<Block> GLOWBERRY_CRATE = BLOCKS.registerSimpleBlock("glowberry_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD).lightLevel((state) -> 13));
+    public static final DeferredBlock<Block> GLOWBERRY_CRATE = BLOCKS.registerBlock("glowberry_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD).lightLevel((state) -> 13));
 
     // Egg Crate
-    public static final DeferredBlock<Block> EGG_CRATE = BLOCKS.registerSimpleBlock("egg_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> EGG_CRATE = BLOCKS.registerBlock("egg_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Brown Egg Crate
-    public static final DeferredBlock<Block> BROWN_EGG_CRATE = BLOCKS.registerSimpleBlock("brown_egg_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> BROWN_EGG_CRATE = BLOCKS.registerBlock("brown_egg_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Blue Egg Crate
-    public static final DeferredBlock<Block> BLUE_EGG_CRATE = BLOCKS.registerSimpleBlock("blue_egg_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> BLUE_EGG_CRATE = BLOCKS.registerBlock("blue_egg_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Peanut Crate (Expanded Delight)
-    public static final DeferredBlock<Block> PEANUT_CRATE = BLOCKS.registerSimpleBlock("peanut_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> PEANUT_CRATE = BLOCKS.registerBlock("peanut_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Salmon Crate
-    public static final DeferredBlock<Block> SALMON_CRATE = BLOCKS.registerSimpleBlock("salmon_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> SALMON_CRATE = BLOCKS.registerBlock("salmon_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Cod Crate
-    public static final DeferredBlock<Block> COD_CRATE = BLOCKS.registerSimpleBlock("cod_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> COD_CRATE = BLOCKS.registerBlock("cod_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Red Mushroom Crate
-    public static final DeferredBlock<Block> RED_MUSHROOM_CRATE = BLOCKS.registerSimpleBlock("red_mushroom_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> RED_MUSHROOM_CRATE = BLOCKS.registerBlock("red_mushroom_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Brown Mushroom Crate
-    public static final DeferredBlock<Block> BROWN_MUSHROOM_CRATE = BLOCKS.registerSimpleBlock("brown_mushroom_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> BROWN_MUSHROOM_CRATE = BLOCKS.registerBlock("brown_mushroom_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Pumpkin Slice Crate
-    public static final DeferredBlock<Block> PUMPKIN_SLICE_CRATE = BLOCKS.registerSimpleBlock("pumpkin_slice_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> PUMPKIN_SLICE_CRATE = BLOCKS.registerBlock("pumpkin_slice_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Golden Carrot Crate
-    public static final DeferredBlock<Block> GOLDEN_CARROT_CRATE = BLOCKS.registerSimpleBlock("golden_carrot_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> GOLDEN_CARROT_CRATE = BLOCKS.registerBlock("golden_carrot_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Golden Apple Crate
-    public static final DeferredBlock<Block> GOLDEN_APPLE_CRATE = BLOCKS.registerSimpleBlock("golden_apple_crate",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
+    public static final DeferredBlock<Block> GOLDEN_APPLE_CRATE = BLOCKS.registerBlock("golden_apple_crate", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.WOOD));
 
     // Bread Bag
-    public static final DeferredBlock<Block> BREAD_BAG = BLOCKS.registerSimpleBlock("bread_bag",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
+    public static final DeferredBlock<Block> BREAD_BAG = BLOCKS.registerBlock("bread_bag", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
 
     // Cookie Bag
-    public static final DeferredBlock<Block> COOKIE_BAG = BLOCKS.registerSimpleBlock("cookie_bag",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
+    public static final DeferredBlock<Block> COOKIE_BAG = BLOCKS.registerBlock("cookie_bag", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
 
     // Cocoa Beans Bag
-    public static final DeferredBlock<Block> COCOABEANS_BAG = BLOCKS.registerSimpleBlock("cocoabeans_bag",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
+    public static final DeferredBlock<Block> COCOABEANS_BAG = BLOCKS.registerBlock("cocoabeans_bag", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
 
     // Sugar Bag
-    public static final DeferredBlock<Block> SUGAR_BAG = BLOCKS.registerSimpleBlock("sugar_bag",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
+    public static final DeferredBlock<Block> SUGAR_BAG = BLOCKS.registerBlock("sugar_bag", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
 
     // Gunpowder Bag
-    public static final DeferredBlock<Block> GUNPOWDER_BAG = BLOCKS.registerSimpleBlock("gunpowder_bag",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
+    public static final DeferredBlock<Block> GUNPOWDER_BAG = BLOCKS.registerBlock("gunpowder_bag", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
 
     // Salt Bag (Expanded Delight)
-    public static final DeferredBlock<Block> SALT_BAG = BLOCKS.registerSimpleBlock("salt_bag",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
+    public static final DeferredBlock<Block> SALT_BAG = BLOCKS.registerBlock("salt_bag", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
 
     // Ground Cinnamon Bag (Expanded Delight)
-    public static final DeferredBlock<Block> GROUND_CINNAMON_BAG = BLOCKS.registerSimpleBlock("ground_cinnamon_bag",
-            BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
+    public static final DeferredBlock<Block> GROUND_CINNAMON_BAG = BLOCKS.registerBlock("ground_cinnamon_bag", Block::new,
+            () -> BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL).mapColor(MapColor.WOOL));
 }
