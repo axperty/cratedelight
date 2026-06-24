d6a672ca911018ce7da415187b1ae0770c28db1b
axperty <107956734+axperty@users.noreply.github.com>
2026-06-24 14:49:08 -0300

    See changelog.md for the latest changes.

--- src/main/java/com/axperty/cratedelight/CrateDelight.java
+++ src/main/java/com/axperty/cratedelight/CrateDelight.java
@@ -6,17 +6,9 @@ import com.axperty.cratedelight.registry.ItemRegistry;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
-
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 @Mod(CrateDelight.MODID)
 public class CrateDelight
@@ -24,33 +16,10 @@ public class CrateDelight
     public static final String MODID = "cratedelight";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public CrateDelight(IEventBus modEventBus, ModContainer modContainer)
-    {
-        modEventBus.addListener(this::commonSetup);
+    public CrateDelight(IEventBus modEventBus, ModContainer modContainer) {
         BlockRegistry.BLOCKS.register(modEventBus);
         ItemRegistry.ITEMS.register(modEventBus);
         CreativeTabRegistry.CREATIVE_MODE_TABS.register(modEventBus);
-        NeoForge.EVENT_BUS.register(this);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-
-    }
-
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-
-        }
+        LOGGER.info("Crate Delight loaded");
     }
 }
